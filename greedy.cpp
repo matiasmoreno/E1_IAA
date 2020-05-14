deeb72effc1782c74f7a07be17cc2b2e64bea7d4
Matias Moreno <matias.moreno@sansano.usm.cl>
2020-05-14 11:17:30 -0400

    Outputs para todas las instancias

--- greedy.cpp
+++ greedy.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <bits/stdc++.h>
+#include <chrono>  // for high_resolution_clock
 
 using namespace std;
 void miope(int& bestTruck, int& bestFarm, int capacity[], int position[], int production[], int **cost, float minPrize, int nTrucks, int nFarms){
@@ -32,286 +33,301 @@ void miope(int& bestTruck, int& bestFarm, int capacity[], int position[], int pr
 
 int main()
 {
-	ifstream inFile;
-	inFile.open("MC/Instances/instancia10.dat");
+	int in;
+	for (in = 1; in < 23; in++){
 
-	string line;
-	string word;
+		// Record start time
+		auto start = std::chrono::high_resolution_clock::now();
 
-	int nFarms, nTrucks, i, j, origin;
-	float minPrize;
+		ifstream inFile;
+		string instance;
+		instance = "instancia" + to_string(in);
+		inFile.open("MC/Instances/" + instance + ".dat");
+		string line;
+		string word;
 
-	// Obtener nodo origin
+		int nFarms, nTrucks, i, j, origin;
+		float minPrize;
 
-	while (inFile >> word)
-	{
-		if (word == "-")
+		// Obtener nodo origin
+
+		while (inFile >> word)
 		{
-			inFile >> word;
-			origin = stoi(word);
-			break;
-		}
-	}
-
-	// Obtener tamaño de arreglos
-
-	// LLegar hasta predios productores
-	while (inFile >> word)
-	{
-		if (word == "I:=")
-		{
-			break;
-		}
-	}
-
-	nFarms = 1;
-    // Recorrer predios
-	while (inFile >> word)
-	{
-		nFarms = nFarms + 1;
-        size_t found = word.find(";");
-        if (found != string::npos) 
-        {
-            break;
-        }
-	}
-
-    // LLegar hasta camiones
-	while (inFile >> word)
-	{
-		if (word == "K:=")
-		{
-			break;
-		}
-	}
-
-    // Recorrer camiones
-    nTrucks = 1;
-	while (inFile >> word)
-	{
-        nTrucks = nTrucks + 1;
-        size_t found = word.find(";");
-        if (found != string::npos) 
-        {
-            break;
-        }
-	}
-
-	int capacity [nTrucks], position [nTrucks], load [nTrucks], production [nFarms];
-	int **cost;
-	cost = new int *[nFarms];
-	for(int i = 0; i < nFarms; i++)
-	{
-    	cost[i] = new int[nFarms];
-	}
-
-	// LLegar hasta #capacidades
-	while (inFile >> word)
-	{
-		if (word == "param")
-		{
-			break;
-		}
-	}
-
-	// Salta 'Q:='
-	getline(inFile, line);
-
-	// Obtener capacidades
-	i = 1;
-	inFile >> word;
-	while (word != ";")
-	{
-		// Capacidad
-		inFile >> word;
-		capacity[i] = stoi(word);
-		// N° Camion o ;
-		inFile >> word;
-		i = i + 1;
-	}
-	
-	// Cantidad a recolectar
-
-    while (inFile >> word)
-	{
-		if (word == "P:=")
-		{
-			inFile >> word;
-			minPrize = stof(word);
-			break;
-		}
-	}
-
-	// LLegar hasta #producción
-	while (inFile >> word)
-	{
-		if (word == "param")
-		{
-			break;
-		}
-	}
-	// Salto qu :=
-	getline(inFile, line);
-	// Salto producción de planta 0
-	getline(inFile, line);
-
-	// Obtener producciones
-	i = 1;
-	inFile >> word;
-	while (word != ";")
-	{
-		// Produccion
-		inFile >> word;
-		production[i] = stoi(word);
-		// N° granja ;
-		inFile >> word;
-		i = i + 1;
-	}
-
-	// LLegar hasta #costos :
-	while (inFile >> word)
-	{
-		if (word == ":")
-		{
-			break;
-		}
-	}
-	// Salto primera fila
-	getline(inFile, line);
-
-	for (i = 0; i < nFarms; i++)
-	{
-		// Salto la primera palabra de cada fila (La que corresponde al nodo de salida)
-		inFile >> word;
-		for (j = 0; j < nFarms; j++)
-		{
-			inFile >> word;
-			if (i == j)
+			if (word == "-")
 			{
-				cost[i][j] = 0;
-			}
-			else
-			{
-				cost[i][j] = stoi(word);
+				inFile >> word;
+				origin = stoi(word);
+				break;
 			}
 		}
-	}
 
-	inFile.close();
+		// Obtener tamaño de arreglos
 
-	// Sets de rutas dinamicos: vector de vectores? arreglo de vectores?
-
-	// Crear 3 vectores dinamicos para generar las rutas
-
-	vector <int> routes[nTrucks];
-
-	// Inicializar posiciones y carga de los camiones en el origen: nodo 0
-
-	for (i = 1; i < nTrucks; i++)
-	{
-		position[i] = 0;
-		load[i] = 0;
-	}
-
-	// Repetir Función añadir optimo local hasta alcanzar minimo de producción
-
-	int bestTruck = 0, bestFarm = 0, actualPrize = 0;
-
-	while (actualPrize < minPrize)
-	{
-		miope(bestTruck, bestFarm, capacity, position, production, cost, minPrize, nTrucks, nFarms);
-		if (routes[bestTruck].size() == 0)
+		// LLegar hasta predios productores
+		while (inFile >> word)
 		{
-			routes[bestTruck].push_back(origin);
-		}
-		routes[bestTruck].push_back(bestFarm);
-		position[bestTruck] = bestFarm;
-		capacity[bestTruck]	= capacity[bestTruck] - production[bestFarm];
-		load[bestTruck] = load[bestTruck] + production[bestFarm];
-		actualPrize = actualPrize + production[bestFarm];
-		production[bestFarm] = 0;
-	}
-	
-	// Volver a la planta
-	int notEmptyRoutes = 0;
-
-	for (i = 1; i < nTrucks; i++)
-	{
-		if (routes[i].size() != 0)
-		{
-			routes[i].push_back(origin);
-			notEmptyRoutes++;
-		}
-	}
-
-	// Calcular costo solución
-
-	int costRoutes = 0;
-	int distance;
-
-	for (i = 1; i < nTrucks; i++)
-	{
-		distance = 0;
-		if (routes[i].size() != 0)
-		{
-			for (j = 0; j < routes[i].size() - 1; j++)
+			if (word == "I:=")
 			{
-				if (routes[i][j] == origin)
+				break;
+			}
+		}
+
+		nFarms = 1;
+		// Recorrer predios
+		while (inFile >> word)
+		{
+			nFarms = nFarms + 1;
+			size_t found = word.find(";");
+			if (found != string::npos) 
+			{
+				break;
+			}
+		}
+
+		// LLegar hasta camiones
+		while (inFile >> word)
+		{
+			if (word == "K:=")
+			{
+				break;
+			}
+		}
+
+		// Recorrer camiones
+		nTrucks = 1;
+		while (inFile >> word)
+		{
+			nTrucks = nTrucks + 1;
+			size_t found = word.find(";");
+			if (found != string::npos) 
+			{
+				break;
+			}
+		}
+
+		int capacity [nTrucks], position [nTrucks], load [nTrucks], production [nFarms];
+		int **cost;
+		cost = new int *[nFarms];
+		for(int i = 0; i < nFarms; i++)
+		{
+			cost[i] = new int[nFarms];
+		}
+		// LLegar hasta #capacidades
+		while (inFile >> word)
+		{
+			if (word == "param")
+			{
+				break;
+			}
+		}
+
+		// Salta 'Q:='
+		getline(inFile, line);
+
+		// Obtener capacidades
+		i = 1;
+		inFile >> word;
+		while (word != ";")
+		{
+			// Capacidad
+			inFile >> word;
+			capacity[i] = stoi(word);
+			// N° Camion o ;
+			inFile >> word;
+			i = i + 1;
+		}
+		
+		// Cantidad a recolectar
+
+		while (inFile >> word)
+		{
+			if (word == "P:=")
+			{
+				inFile >> word;
+				minPrize = stof(word);
+				break;
+			}
+		}
+		// LLegar hasta #producción
+		while (inFile >> word)
+		{
+			if (word == "param")
+			{
+				break;
+			}
+		}
+		// Salto qu :=
+		getline(inFile, line);
+		// Salto producción de planta 0
+		getline(inFile, line);
+
+		// Obtener producciones
+		i = 1;
+		inFile >> word;
+		while (word != ";")
+		{
+			// Produccion
+			inFile >> word;
+			production[i] = stoi(word);
+			// N° granja ;
+			inFile >> word;
+			i = i + 1;
+		}
+		// LLegar hasta #costos :
+		while (inFile >> word)
+		{
+			if (word == ":")
+			{
+				break;
+			}
+		}
+		// Salto primera fila
+		getline(inFile, line);
+
+		for (i = 0; i < nFarms; i++)
+		{
+			// Salto la primera palabra de cada fila (La que corresponde al nodo de salida)
+			inFile >> word;
+			for (j = 0; j < nFarms; j++)
+			{
+				inFile >> word;
+				if (i == j)
 				{
-					costRoutes = costRoutes + cost[0][routes[i][j + 1]];
-					distance = distance + cost[0][routes[i][j + 1]];
-				}
-				else if (routes[i][j + 1] == origin)
-				{
-					costRoutes = costRoutes + cost[routes[i][j]][0];
-					distance = distance + cost[routes[i][j]][0];
+					cost[i][j] = 0;
 				}
 				else
 				{
-					costRoutes = costRoutes + cost[routes[i][j]][routes[i][j + 1]];
-					distance = distance + cost[routes[i][j]][routes[i][j + 1]];
-				}	
+					cost[i][j] = stoi(word);
+				}
 			}
 		}
-		if (routes[i].size() != 0)
+
+		inFile.close();
+
+		// Sets de rutas dinamicos: vector de vectores? arreglo de vectores?
+
+		// Crear 3 vectores dinamicos para generar las rutas
+
+		vector <int> routes[nTrucks];
+
+		// Inicializar posiciones y carga de los camiones en el origen: nodo 0
+
+		for (i = 1; i < nTrucks; i++)
 		{
-			routes[i].push_back(distance);
+			position[i] = 0;
+			load[i] = 0;
 		}
-	}
 
-	// Printear solución y costo asociado
+		// Repetir Función añadir optimo local hasta alcanzar minimo de producción
 
-	cout << costRoutes << " " << actualPrize << " " << notEmptyRoutes << endl;
+		int bestTruck = 0, bestFarm = 0, actualPrize = 0;
 
-	for (i = 1; i < nTrucks; i++)
-	{
-		if (routes[i].size() != 0)
-		{	
-			cout << "Truck " << i << " ";
-			for (j = 0; j < routes[i].size() - 1; j++)
+		while (actualPrize < minPrize)
+		{
+			miope(bestTruck, bestFarm, capacity, position, production, cost, minPrize, nTrucks, nFarms);
+			if (routes[bestTruck].size() == 0)
 			{
-				if (routes[i][j] == origin)
+				routes[bestTruck].push_back(origin);
+			}
+			routes[bestTruck].push_back(bestFarm);
+			position[bestTruck] = bestFarm;
+			capacity[bestTruck]	= capacity[bestTruck] - production[bestFarm];
+			load[bestTruck] = load[bestTruck] + production[bestFarm];
+			actualPrize = actualPrize + production[bestFarm];
+			production[bestFarm] = 0;
+		}
+		
+		// Volver a la planta
+		int notEmptyRoutes = 0;
+
+		for (i = 1; i < nTrucks; i++)
+		{
+			if (routes[i].size() != 0)
+			{
+				routes[i].push_back(origin);
+				notEmptyRoutes++;
+			}
+		}
+
+		// Calcular costo solución
+
+		int costRoutes = 0;
+		int distance;
+
+		for (i = 1; i < nTrucks; i++)
+		{
+			distance = 0;
+			if (routes[i].size() != 0)
+			{
+				for (j = 0; j < routes[i].size() - 1; j++)
 				{
-					if (j == 0)
+					if (routes[i][j] == origin)
 					{
-						cout << origin << "-";
+						costRoutes = costRoutes + cost[0][routes[i][j + 1]];
+						distance = distance + cost[0][routes[i][j + 1]];
+					}
+					else if (routes[i][j + 1] == origin)
+					{
+						costRoutes = costRoutes + cost[routes[i][j]][0];
+						distance = distance + cost[routes[i][j]][0];
 					}
 					else
 					{
-						cout << origin << " " << routes[i][j + 1] << " " << load[i] << endl;
+						costRoutes = costRoutes + cost[routes[i][j]][routes[i][j + 1]];
+						distance = distance + cost[routes[i][j]][routes[i][j + 1]];
 					}	
 				}
-				else
-				{
-					cout << routes[i][j] << "-";
-				}	
+			}
+			if (routes[i].size() != 0)
+			{
+				routes[i].push_back(distance);
 			}
 		}
+
+		// Printear solución y costo asociado
+
+		ofstream outFile;
+		outFile.open ("outputs/" + instance + ".txt");
+		
+
+		outFile << costRoutes << " " << actualPrize << " " << notEmptyRoutes << endl;
+		// cout << costRoutes << " " << actualPrize << " " << notEmptyRoutes << endl;
+
+		for (i = 1; i < nTrucks; i++)
+		{
+			if (routes[i].size() != 0)
+			{	
+				// cout << "Truck " << i << " ";
+				outFile << "Truck " << i << " ";
+				for (j = 0; j < routes[i].size() - 1; j++)
+				{
+					if (routes[i][j] == origin)
+					{
+						if (j == 0)
+						{
+							// cout << origin << "-";
+							outFile << origin << "-";
+						}
+						else
+						{
+							// cout << origin << " " << routes[i][j + 1] << " " << load[i] << endl;
+							outFile << origin << " " << routes[i][j + 1] << " " << load[i] << endl;
+						}	
+					}
+					else
+					{
+						// cout << routes[i][j] << "-";
+						outFile << routes[i][j] << "-";
+					}	
+				}
+			}
+		}
+		// Record end time
+		auto finish = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> elapsed = finish - start;
+		// std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+		outFile << "Elapsed time: " << elapsed.count() << " s\n";
+		outFile.close();
 	}
-
-	// Print de vicente
-	// función de evaluación, total de leche recolectado, cantidad de rutas
-	// rutas, distancia recorrida, leche recogida
-
 	return 0;
 }
